d9ce486b3c6a611b67a5d1e215bfec6f48e9500d
Prashant Acharya <prashant.acharya@sagarmatha.edu.np>
2020-08-05 22:23:56 +0545

    Updates

---
+++ precache-manifest.103e1af6816c5626f2072cc37a9ccdf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee5fe358cd1a5753a15fd3f38c4c4a02",
+    "url": "/github-ui/index.html"
+  },
+  {
+    "revision": "edc94271a407370057d2",
+    "url": "/github-ui/static/css/main.3f126160.chunk.css"
+  },
+  {
+    "revision": "386244a12989f2f771bf",
+    "url": "/github-ui/static/js/2.3514db5f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-ui/static/js/2.3514db5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edc94271a407370057d2",
+    "url": "/github-ui/static/js/main.df812cb5.chunk.js"
+  },
+  {
+    "revision": "306134a982c6fc4fa389",
+    "url": "/github-ui/static/js/runtime-main.b7cf24d9.js"
+  }
+]);
